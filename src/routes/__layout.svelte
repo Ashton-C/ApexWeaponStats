6e517ea9ac7016f787873de357f7198eacd00656
Ashton Christensen <armadillomaster@gmail.com>
2021-07-15 22:41:36 -0700

    Worked on main lists generating lists of weapons

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,8 @@
+<nav>
+    <h2>ApexWeaponStats</h2>
+    <div class='navButtons'>
+        <a class='navButton-default' href='./weaponstats'>Weapon Icon</a>
+        <a class='navButton-default' href='./contactme'>Contact Icon</a>
+    </div>
+</nav>
+<slot></slot>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+import { loop_guard } from "svelte/internal";
+
+
+    export let weaponClasses = ['Assualt', 'SMG', 'Sniper'];
+    export let weaponsAssualt = ['Flatline', 'R-301'];
+    export let weaponsSMG = ['R-99', 'Volt']
+    export let weaponsSniper = ['Sentinel', 'Longbow']
+</script>
+
+
+<h1 class='header'>Welcome to ApexWeaponStats</h1>
+<div class='weaponNavbuttons-full'>
+    <ol class='weaponClasses'>
+        {#each weaponClasses as wepclass}
+            <li class='weaponClass {wepclass}'>
+                <ul class='weapons-{wepclass}'>
+                    {#each weaponsSniper as weapon}
+                        <li class='weapon {weapon}'>{weapon} Icon</li>
+                    {/each}
+                </ul>
+            </li>
+        {/each}
+        <li class='weaponClass assault'>
+            <ul class='weapons-assualt'>
+                <li class='weapon flatline'>Flatline Icon</li>
+                <li class='weapon r301'>R-301 Icon</li>
+            </ul>
+        </li>
+        <li class='weaponClass sniper'>
+            <ul class='weapons-sniper'>
+                <li class='weapon sentinel'>Sentinel Icon</li>
+                <li class='weapon longbow'>Longbow Icon</li>
+            </ul>
+        </li>
+        <li class='weaponClass submachine'>
+            <ul class='weapons-submachine'>
+                <li class='weapon r-99'>R99 Icon</li>
+                <li class='weapon volt'>Volt Icon</li>
+            </ul>
+        </li>
+    </ol>
+</div>
+
